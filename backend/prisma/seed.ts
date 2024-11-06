0fbadea896d4b4b387d3289c486c05a6e39d4b8a
Benjaaaamín <benja_e_e@hotmail.com>
2024-11-06 13:16:11 -0300

    up

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -4,13 +4,13 @@ const prisma = new PrismaClient();
 
 async function main() {
   // Crear un paciente
-  const paciente1 = await prisma.paciente.create({
+  const paciente1 = await prisma.patient.create({
     data: {
-      usuario: {
+      user: {
         create: {
           email: 'paciente2@example.com',
           password: 'contraseña2',
-          tipoUsuario: 'PACIENTE',
+          userType: 'PACIENTE',
           contact: {
             create: {
               telephone: '321321321',
@@ -21,7 +21,7 @@ async function main() {
           },
         },
       },
-      historialMedico: {
+      medicalHistory: {
         create: {
           descripcion: 'Alergia a la penicilina',
           // otros campos del historial médico...
@@ -30,9 +30,9 @@ async function main() {
       exams: {
         create: [
           {
-            nombre: 'Examen de sangre',
-            numero_orden: 1,
-            contenido: 'Resultado de hemoglobina y otros parámetros.',
+            name: 'Examen de sangre',
+            orderNumber: 1,
+            content: 'Resultado de hemoglobina y otros parámetros.',
             format: 'PDF',
           },
         ],
@@ -41,14 +41,14 @@ async function main() {
   });
 
   // Crear un médico
-  const medico1 = await prisma.medico.create({
+  const medico1 = await prisma.doctor.create({
     data: {
       especialidad: 'Pediatría',
       usuario: {
         create: {
           email: 'medico1@example.com',
           password: 'contraseña3',
-          tipoUsuario: 'MEDICO',
+          userType: 'MEDICO',
           contact: {
             create: {
               telephone: '321321321',
@@ -64,9 +64,9 @@ async function main() {
           {
             entidad: {
               create: {
-                nombre: 'Hospital General',
-                direccion: 'Calle Principal 123',
-                telefono: '555555555',
+                name: 'Hospital General',
+                address: 'Calle Principal 123',
+                telephone: '555555555',
                 tipoEntidad: 'PUBLICO',
               },
             },
@@ -93,11 +93,11 @@ async function main() {
   // Crear un examen
   const exam1 = await prisma.exam.create({
     data: {
-      nombre: 'Radiografía de tórax',
-      numero_orden: 2,
-      contenido: 'Radiografía para evaluar el estado de los pulmones.',
+      name: 'Radiografía de tórax',
+      orderNumber: 2,
+      content: 'Radiografía para evaluar el estado de los pulmones.',
       format: 'IMAGEN',
-      paciente: {
+      patient: {
         connect: { id: paciente1.id },
       },
     },
@@ -106,9 +106,9 @@ async function main() {
   // Crear una entidad
   const entidad1 = await prisma.entidad.create({
     data: {
-      nombre: 'Clínica de Salud',
-      direccion: 'Avenida Secundaria 456',
-      telefono: '777777777',
+      name: 'Clínica de Salud',
+      address: 'Avenida Secundaria 456',
+      telephone: '777777777',
       tipoEntidad: 'PRIVADO',
       prestaciones: {
         create: [
@@ -134,14 +134,14 @@ async function main() {
   });
 
   // Crear un administrador
-  const administrador1 = await prisma.administrador.create({
+  const administrador1 = await prisma.admin.create({
     data: {
-      departamento: 'Recursos Humanos',
-      usuario: {
+      department: 'Recursos Humanos',
+      user: {
         create: {
           email: 'admin1@example.com',
           password: 'contraseña4',
-          tipoUsuario: 'ADMINISTRADOR',
+          userType: 'ADMINISTRADOR',
           contact: {
             create: {
               telephone: '321321321',
@@ -188,39 +188,39 @@ async function main() {
   console.log("Categorías creadas:", categorias);
 
   // medicamentos
-  const medicamentos = await prisma.medicamento.createMany({
+  const medicamentos = await prisma.medicine.createMany({
     data: [
       {
         name: "Amoxicilina",
-        dosis: "500 mg cada 8 horas",
-        tipo: "Antibiótico",
-        descripcion: "Utilizado para tratar diversas infecciones bacterianas.",
-        efectosSecundarios: "Náuseas, erupciones cutáneas.",
-        contraindicaciones: "Alergia a la penicilina.",
+        dosage: "500 mg cada 8 horas",
+        type: "Antibiótico",
+        description: "Utilizado para tratar diversas infecciones bacterianas.",
+        sideEffects: "Náuseas, erupciones cutáneas.",
+        contraindications: "Alergia a la penicilina.",
       },
       {
         name: "Ibuprofeno",
-        dosis: "400 mg cada 6 horas",
-        tipo: "Analgésico",
-        descripcion: "Usado para aliviar el dolor y reducir la inflamación.",
-        efectosSecundarios: "Malestar estomacal, mareos.",
-        contraindicaciones: "Úlceras gástricas.",
+        dosage: "400 mg cada 6 horas",
+        type: "Analgésico",
+        description: "Usado para aliviar el dolor y reducir la inflamación.",
+        sideEffects: "Malestar estomacal, mareos.",
+        contraindications: "Úlceras gástricas.",
       },
       {
         name: "Paracetamol",
-        dosis: "500 mg cada 8 horas",
-        tipo: "Analgésico",
-        descripcion: "Medicamento común para el alivio del dolor.",
-        efectosSecundarios: "Pocas reacciones adversas, generalmente seguro.",
-        contraindicaciones: "Enfermedades hepáticas severas.",
+        dosage: "500 mg cada 8 horas",
+        type: "Analgésico",
+        description: "Medicamento común para el alivio del dolor.",
+        sideEffects: "Pocas reacciones adversas, generalmente seguro.",
+        contraindications: "Enfermedades hepáticas severas.",
       },
       {
         name: "Sertralina",
-        dosis: "50 mg una vez al día",
-        tipo: "Antidepresivo",
-        descripcion: "Usado para tratar trastornos depresivos y de ansiedad.",
-        efectosSecundarios: "Náuseas, insomnio.",
-        contraindicaciones: "Alergias a inhibidores selectivos de la recaptación de serotonina.",
+        dosage: "50 mg una vez al día",
+        type: "Antidepresivo",
+        description: "Usado para tratar trastornos depresivos y de ansiedad.",
+        sideEffects: "Náuseas, insomnio.",
+        contraindications: "Alergias a inhibidores selectivos de la recaptación de serotonina.",
       },
     ],
   });
@@ -228,48 +228,18 @@ async function main() {
   console.log("Medicamentos creados:", medicamentos);
 
   // analitos
-  const analitos = await prisma.analito.createMany({
+  const analitos = await prisma.analyte.createMany({
     data: [
       {
-        nombre: "Glucosa",
-        unidades: "mg/dL",
-        rangoNormal: "70-100",
+        parameter: "Glucosa",
+        units: "mg/dL",
+        normalRange: "70-100",
       },
       {
-        nombre: "Colesterol Total",
-        unidades: "mg/dL",
-        rangoNormal: "125-200",
-      },
-      {
-        nombre: "Triglicéridos",
-        unidades: "mg/dL",
-        rangoNormal: "50-150",
-      },
-      {
-        nombre: "HDL (Colesterol Bueno)",
-        unidades: "mg/dL",
-        rangoNormal: "40-60",
-      },
-      {
-        nombre: "LDL (Colesterol Malo)",
-        unidades: "mg/dL",
-        rangoNormal: "<130",
-      },
-      {
-        nombre: "Creatinina",
-        unidades: "mg/dL",
-        rangoNormal: "0.6-1.2",
-      },
-      {
-        nombre: "Bilirrubina Total",
-        unidades: "mg/dL",
-        rangoNormal: "0.1-1.2",
-      },
-      {
-        nombre: "Ácido Úrico",
-        unidades: "mg/dL",
-        rangoNormal: "3.5-7.2",
-      },
+        parameter: "Colesterol Total",
+        units: "mg/dL",
+        normalRange: "125-200",
+      }
     ],
   });
 
